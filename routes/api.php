4545e04fb36615ada896ce7867b35e328f685a36
unknown <nyandiakamau24@gmail.com>
2023-12-02 11:36:33 +0300

    changes

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,19 @@
 // routes/api.php
 
 use App\Http\Controllers\TodoController;
+//ADDED
+use App\Http\Controllers\AuthController;
+
 
 Route::resource('todos', TodoController::class);
-//Route::get('/todos', [TodoController::class, 'index']);
+Route::get('/todos', [TodoController::class, 'index']);
 Route::get('/todos/{id}', [TodoController::class, 'show']);
 Route::post('/todos', [TodoController::class, 'store']);
 Route::put('/todos/{id}', [TodoController::class, 'update']);
 Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+// Protect the logout route with Sanctum's auth middleware
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
