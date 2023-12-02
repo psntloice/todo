5f10df52c9086b3085ac3c03adda77ff78651aae
unknown <nyandiakamau24@gmail.com>
2023-12-02 13:58:04 +0300

    changes

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,16 @@
 use App\Http\Controllers\TodoController;
 //ADDED
 use App\Http\Controllers\AuthController;
+Route::middleware(['auth'])->group(function () {
+    Route::get('/todos', [TodoController::class, 'index'])->name('todos.index');
+    Route::get('/todos/create', [TodoController::class, 'create'])->name('todos.create');
+    Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
+    Route::get('/todos/{todo}', [TodoController::class, 'show'])->name('todos.show');
+    Route::get('/todos/{todo}/edit', [TodoController::class, 'edit'])->name('todos.edit');
+    Route::put('/todos/{todo}', [TodoController::class, 'update'])->name('todos.update');
+    Route::delete('/todos/{todo}', [TodoController::class, 'destroy'])->name('todos.destroy');
+});
 
-
-Route::resource('todos', TodoController::class);
-Route::get('/todos', [TodoController::class, 'index']);
-Route::get('/todos/{id}', [TodoController::class, 'show']);
-Route::post('/todos', [TodoController::class, 'store']);
-Route::put('/todos/{id}', [TodoController::class, 'update']);
-Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
