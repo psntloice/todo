ace501756dc2631ad1a00c8cf6a27add425e5020
unknown <nyandiakamau24@gmail.com>
2023-12-12 00:49:39 +0300

    changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,30 +17,7 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-      /*  $field = filter_var($request->input('login'), FILTER_VALIDATE_EMAIL)
-            ? 'email'
-            : 'phone';
-
-        $request->merge([$field => $request->input('login')]);
-
-        $credentials = $request->only($field, 'password');
-
-        if (auth()->attempt($credentials)) {
-            //return $this->issueToken($request);
-            $token = Auth::user()->createToken('auth_token')->plainTextToken;
-
-            return response()->json(['token' => $token]);
-        }
-
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-
-        return response()->json(['error' => 'Unauthorized'], 401);
-
-        /* */
-
-        // Validate the incoming request data
+       // Validate the incoming request data
         $validator = Validator::make($request->all(), [
             'login_type' => 'required|in:email,phone',
             'identifier' => [
@@ -51,122 +28,20 @@ class AuthController extends Controller
             'password' => 'required|string',
         ]);
 
+        // Check if validation fails
         if (Auth::attempt(['email' => $request->input('identifier'), 'password' => $request->input('password')]) ||
         Auth::attempt(['phone' => $request->input('identifier'), 'password' => $request->input('password')])) {
         // Authentication successful
-        $token = Auth::user()->createToken('auth_token')->plainTextToken;
-
-            return response()->json(['token' => $token]);
-        return redirect()->intended('/dashboard'); // Redirect to the intended page after login
-    } else {
+            $token = Auth::user()->createToken('auth_token')->plainTextToken;
+            $message = 'Validation successful';
+            return response()->json(['token' => $token, 'message' => $message,]);
+         } else {
         // Authentication failed
         throw new ValidationException($validator);
-        return redirect()->back()->withInput($request->only('identifier'))->withErrors(['identifier' => 'Invalid login credentials']);
+       // return redirect()->back()->withInput($request->only('identifier'))->withErrors(['identifier' => 'Invalid login credentials']);
+            }      
     }
-
-        // Check if validation fails
-        if ($validator->fails()) {
-            throw new ValidationException($validator);
-        }
-        else{
-            $token = Auth::user()->createToken('auth_token')->plainTextToken;
-
-            return response()->json(['token' => $token]);
-        }
-//trial data
-/*{
-  "login_type": "phone",
-  //"login_type": "email",
- // "identifier": "mary@gmail.com",
-  "identifier": "+254709581700",
-  "password": "mukabwaloice"
-}*/
-        // If the validation passes, continue with your logic
-        // ...
-
-        // Return a success response
-        return response()->json(['message' => 'Validation successful']);
-    
-/*
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-*/
-      /*  if (Auth::attempt($request->only('email', 'password'))) {
-            $token = Auth::user()->createToken('auth_token')->plainTextToken;
-
-            return response()->json(['token' => $token]);
-        }
-
-        if (Auth::attempt($request->only('phone', 'password'))) {
-            $token = Auth::user()->createToken('auth_token')->plainTextToken;
-
-            return response()->json(['token' => $token]);
-        }
-
-        // Create a validator instance
-$validator = Validator::make($request, $rules);
-
-// Check if validation fails
-if ($validator->fails()) {
-    throw new ValidationException($validator);
-}
-        /*throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);*/
-        //
-    }
-
-    //added theese
-   
-
-    // ... other methods ...
-
-    /**
-     * Validate the user login request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return void
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    protected function validateLogin(Request $request)
-    {
-        $request->validate([
-            $this->username() => 'required|regex:/^\+[0-9]{1,15}$/',
-            'password' => 'required|string',
-        ], [
-            $this->username() . '.regex' => 'Invalid phone number format.',
-        ]);
-    }
-
-    /**
-     * Attempt to log the user into the application.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
-   /* protected function attemptLogin(Request $request)
-    {
-        $field = filter_var($request->input($this->username()), FILTER_VALIDATE_INT)
-            ? 'phone'
-            : 'email';
-
-        $request->merge([$field => $request->input($this->username())]);
-
-        if ($this->guard()->attempt(
-            $this->credentials($request), $request->filled('remember')
-        )) {
-            return true;
-        }
-
-        throw ValidationException::withMessages([
-            $this->username() => [trans('auth.failed')],
-        ]);
-    }
-*/
-    // ... other methods ...
-
+  
     public function register(Request $request)
     {
         $request->validate([
@@ -174,19 +49,19 @@ if ($validator->fails()) {
             //'email' => 'required|email|unique:users',
             'password' => 'required|min:8',
             'email' => 'sometimes|required|email|unique:users',
-    'phone' => [
+             'phone' => [
         'sometimes',
         'required',
         'unique:users',
         'regex:/^\+[0-9]{1,15}$/',
-    ],
+             ],
         ]);
 
         $user = User::create([
             'name' => $request->input('name'),
             'email' => $request->input('email'),
             'password' => bcrypt($request->input('password')),
-    'phone' => $this->formatPhoneNumber($request->input('phone')),
+         'phone' => $this->formatPhoneNumber($request->input('phone')),
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
