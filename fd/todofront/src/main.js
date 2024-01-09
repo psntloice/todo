d19295449a7e3336f6eeb3737141ea2c0f0b07a4
psntloice <loice.sk@gmail.com>
2024-01-09 15:05:27 +0300

    changes

--- fd/todofront/src/main.js
+++ fd/todofront/src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue'
 import HomePage from './HomePage.vue';
 import PhotoPage from './PhotoPage.vue';
 import landingPage from './landingPage.vue';
+import Hero2 from './Hero2.vue';
 import Hero from './Hero.vue';
 
 //Vue.use(VueRouter)
@@ -16,6 +17,7 @@ const router = createRouter({
         { path: '/', name: 'home', component: HomePage },
         { path: '/photos/:photoId', name: 'photo-details', component: PhotoPage },
         { path: '/landing', name: 'landing', component: landingPage },
+        { path: '/hero2', name: 'hero2', component: Hero2 },
         { path: '/hero', name: 'hero', component: Hero },
       ],  });
   
