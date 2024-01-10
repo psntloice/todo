a41bb2ab29906beadd7e572d6536fa88e5300172
psntloice <loice.sk@gmail.com>
2024-01-10 14:39:40 +0300

    changes

--- fd/todofront/src/router/index.js
+++ fd/todofront/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingView from '../views/LandingView.vue'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
 
 
 const router = createRouter({
@@ -10,6 +12,13 @@ const router = createRouter({
       name: 'landing',
       component: LandingView
     },
+        
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    
     
   ]
 })
